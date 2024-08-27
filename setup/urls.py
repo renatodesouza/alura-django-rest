8217ff7ca34f2b12d2387c6a4f29de1aca7bc848
Renato de souza lucas <42358704+renatodesouza@users.noreply.github.com>
2024-08-26 21:01:35 -0300

    Criacao dos rotas através do routers

--- setup/urls.py
+++ setup/urls.py
@@ -15,8 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from escola.views import EstudanteViewSet, CursoViewSet
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register('estudantes', EstudanteViewSet, basename='Estudantes')
+router.register('cursos', CursoViewSet, basename='Cursos')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(router.urls))
 ]
